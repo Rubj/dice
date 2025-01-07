2f2aa89073d04e2ec038eeff71677d7bab413d1a
saamel <saamel.sonn@gmail.com>
2025-01-07 10:04:25 +0200

    viskeid jöönud

--- node_2d.gd
+++ node_2d.gd
@@ -1,37 +1,56 @@
 extends Node2D
 
-var state: String = "veereta"; #"pood" | "no-input"
+var state_just_changed: bool = true;
+var state: String = "lahing"; #"pood" | "no-input" | ...
 var myDice: Array[Taring] = [];
+var viskeid: int;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#todo load from savefile
 	myDice = [Taring.new(), Taring.new(), Taring.new()];
-
+	viskeid = 5; #todo move to battle init
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if state == "no-input":
 		pass
 	
-	if state == "veereta":
-		stateVeeretaTick(Input.is_action_just_released("click"));
+	if state == "lahing":
+		stateVeeretaTick(Input.is_action_just_released("click"), viskeid);
 	elif state == "pood":
-		statePoodTick();
+		statePoodTick(state_just_changed);
 	#elif state ==
+	
+	#if state_just_changed:
+	#	state_just_changed = false;
 
-func stateVeeretaTick(vise: bool):
+
+func stateVeeretaTick(vise: bool, viskeidJaanud):
+	if viskeidJaanud < 1:
+		state = "no-input";
+		battleEnd()
+		return;
 	if vise:
 		veereta();
 
 func veereta():
+	viskeid -= 1;
 	#todo play animation
 	veeretaTaringuid(myDice);
 
 func veeretaTaringuid(ds: Array[Taring]):
 	for d in ds:
 		d.roll();
-		print(d.current_side);
+	print(str(ds.map(func(d): return d.current_side)) + " || viskeid jäänud: " + str(viskeid));
+#func battleInit
+func battleEnd():
+	state_just_changed = true;
+	state = "pood" #todo handle next state (map to choose?) stateHandler class
 
-func statePoodTick():
+
+func statePoodTick(enter: bool):
+	if enter:
+		state_just_changed = false;
+		print("astusid poodi!");
 	pass
