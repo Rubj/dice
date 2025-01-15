9fce2edc6b989e0fbd039d70fd0d48166960627c
saamel <saamel.sonn@gmail.com>
2025-01-15 20:26:50 +0200

    dice outline for selecting. mouse hovering detection not working

---
+++ scenes/d_6_esimene_katsetus.gd
@@ -0,0 +1,26 @@
+extends MeshInstance3D
+
+var mouse_is_on: bool = false;
+var outline: MeshInstance3D;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	outline = get_child(1);
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_static_body_3d_input_event(camera, event, event_position, normal, shape_idx):
+	print(event) #TODO ARE SIGNALS NOT WORKING BECAUSE OF SUBVIEWPORT???
+
+func _on_static_body_3d_mouse_entered():
+	mouse_is_on = true;
+	print(outline.is_visible_in_tree())
+	outline.set_visible(true);
+
+func _on_static_body_3d_mouse_exited():
+	mouse_is_on = false;
+	outline.set_visible(false);
+	print(outline.is_visible_in_tree())
